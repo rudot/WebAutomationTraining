b882e3e4a0b9b926c8b801e47b33485ad8989fe8
rudot <rudotz@gmail.com>
2019-10-22 10:17:37 +0700

    2nd commit

--- src/test/java/com/rudot/TravelokaTraining/HomePage.java
+++ src/test/java/com/rudot/TravelokaTraining/HomePage.java
@@ -5,11 +5,14 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class HomePage {
+	
+	public static @FindBy(id = "desktop-topbar") WebElement topBar;
 	
 	public static void main(String[] args) {
 		
@@ -18,9 +21,7 @@ public class HomePage {
 		WebDriverWait wait =  new WebDriverWait(driver, 15);
 		driver.manage().window().setSize(new Dimension(1600,900));
 		driver.get("http://www.traveloka.com");
-		WebElement header = driver.findElement(By.id("desktop-topbar"));
-		wait.until(ExpectedConditions.visibilityOf(header));
-		System.out.println("test");
+		wait.until(ExpectedConditions.visibilityOf(topBar));
 	}
 
 }
